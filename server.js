f93d5ce7819be7be28e81342f6726878eca751cd
Saniya1976 <saniyakumari1976@gmail.com>
2025-06-21 18:41:31 +0530

    Initial commit

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ server.get('/', (req, res) => {
 //  Mount your user routes here:
 const userRoutes = require('./routes/user.routes.js');
 server.use('/users', userRoutes);
-
-// THEN start listening:
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
